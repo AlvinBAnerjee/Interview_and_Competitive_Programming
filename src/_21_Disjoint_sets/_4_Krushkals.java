211e54add9678160f8e0295e8710447e8217e306
Alvin Banerjee <alvinbanerjee@Alvins-MacBook-Air.local>
2026-07-09 22:37:41 +0530

    Update with Correct Krushkal code

--- src/_21_Disjoint_sets/_4_Krushkals.java
+++ src/_21_Disjoint_sets/_4_Krushkals.java
@@ -6,119 +6,55 @@ import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class _4_Krushkals {
-    public static class Edge {
-        int u;
-        int v;
-        int weight;
-
-
-        Edge(int u, int v,int weight) {
-            this.u =u;
-            this.weight = weight;
-            this.v = v;
-        }
-    }
-
-    public static class Graph {
-        int V;
-        ArrayList<Edge> E;
-        Graph(int V)
-        {
-            this.V=V;
-            this.E=new ArrayList<>();
-
-        }
-        void addEdge(int u,int v, int weight)
-        {
-            E.add(new Edge(u,v,weight));
-        }
-    }
-    static class DisjointSet{
-        int parent[];
-        int rank[];
-        DisjointSet(int n)
-        {
-            parent=new int[n];
-            rank=new int[n];
-            for (int i=0;i<n;i++) {
-                parent[i] = i;
-                rank[i]=0;
-            }
-        }
-        int find(int x)
-        {
-            if (parent[x]==x)
-                return x;
-            parent[x]= find(parent[x]);
-            return parent[x];
-        }
-        void union(int x,int y)
-        {
-            int x_top=find(x);
-            int y_top=find(y);
-            if (rank[x_top]>rank[y_top])
-            {
-                parent[y_top]=x_top;
-            }
-            else if (rank[y_top]>rank[x_top])
-            {
-                parent[x_top]=y_top;
-            }
-            else
-            {
-                parent[x_top]=y_top;
-                rank[y_top]++;
-
-            }
-
-        }
-    }
-    public static class sort_with_weight implements Comparator<Edge>
+    int parent[];
+    public int find(int x)
     {
-        @Override
-        public int compare(Edge o1, Edge o2) {
-            return o1.weight-o2.weight;
-        }
+        if(x!=parent[x])
+            parent[x]=find(parent[x]);
+        return parent[x];
     }
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int V=sc.nextInt();
-        int E=sc.nextInt();
-        Graph graph=new Graph(V);
-        for (int i=0;i<E;i++)
-        {
-            int u=sc.nextInt();
-            int v=sc.nextInt();
-            int weight=sc.nextInt();
-            graph.addEdge(u,v,weight);
-        }
-        Graph tree=new Graph(V);
-        System.out.println(findMST(graph,tree));
-
-    }
-    static  int findMST(Graph graph,Graph tree)
+    public void union(int u, int v)
     {
-        PriorityQueue<Edge> pq=new PriorityQueue<>(new sort_with_weight());
-        DisjointSet ds=new DisjointSet(graph.V);
-        pq.addAll(graph.E);
-        int total_weight=0;
-        while (!pq.isEmpty())
-        {
-            Edge e=pq.remove();
-            int source=e.u;
-            int destination=e.v;
-            int weight=e.weight;
-            int find_s=ds.find(source);
-            int find_d=ds.find(destination);
-            if (find_s!=find_d)
-            {
-                ds.union(source,destination);
-                total_weight=total_weight+weight;
-                tree.addEdge(source,destination,weight);
-            }
-        }
-        return total_weight;
+        int l = find(u);
+        int r = find(v);
+        parent[l]=r;
+    }
+    public int findMST(int n, int edges[][], int block, int include)
+    {
 
+        for(int i=0;i<n;i++)
+            parent[i]=i;
+        int mst=0;
+        int edgesCount = 0;
+        if(include!=-1)
+        {
+            int u=edges[include][1];
+            int v=edges[include][2];
+            int w=edges[include][3];
+            union(u,v);
+            mst+=w;
+            edgesCount++;
+        }
+        for(int i=0;i<edges.length;i++)
+        {
+            if(i==block)continue;
+            int u=edges[i][1];
+            int v=edges[i][2];
+            int w=edges[i][3];
+            if(find(u)!=find(v))
+            {
+                mst+=w;
+                union(u,v);
+                edgesCount++;
+            }
+
+            if(edgesCount==n-1)break;
+        }
+        for (int i = 0; i < n; i++) {
+            if (find(i) != find(0))
+                return Integer.MAX_VALUE;
+        }
+        return mst;
     }
 
 }
